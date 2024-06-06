fc81aaa7e0d2fbbe6bb7d742835c412125b6e360
Енотайзер <amisitysu14@gmail.com>
2024-06-06 16:10:22 +0300

    first commit

--- src/com/amisitysu/gmail/TaxCalculator.java
+++ src/com/amisitysu/gmail/TaxCalculator.java
@@ -1,22 +1,28 @@
 package com.amisitysu.gmail;
 
+import java.util.Scanner;
+
 public class TaxCalculator {
     public static void main(String[] args) {
-        double income = 11000; // Ваш дохід
+        Scanner scanner = new Scanner(System.in);
+        double income = 11100;
+
 
         double tax;
         if (income <= 10000) {
             tax = income * 0.025;
 
         } else if (income <= 25000) {
-            tax = (income - 10000) * 0.043; // Податок для другого діапазону
+            tax = income * 0.043;
 
         } else {
-            tax = (income - 25000) * 0.067; // Податок для третього діапазону
+            tax = income * 0.067;
 
         }
-        System.out.println("Сума податку: " + tax);
         System.out.println("Сума доходу: " + income);
+        System.out.printf("Сума податку: %.2f", tax);
+
+        scanner.close();
 
     }
 }
